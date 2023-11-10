37ed854e0476854e630ccbab66cb251fa3b3683b
Rafaella <Rafaellamoliveira2005@gmail.com>
2023-11-10 10:15:12 -0300

    Adição de comentários ao código fonte

--- hello.c
+++ hello.c
@@ -2,9 +2,14 @@
 /*
 inblock(em bloco)
 */
+//incluindo a biblioteca de entrada e saída padrão(stdio)
 #include <stdio.h>
+
+//Função main(principal) responsável pela execução do arquivo
 int main(){
+    //comando printf, exibe a mensagem hello world em tela
    printf("hello world!");
+   //retorno para a função main que foi declarada como int
      return 0;
 }
 
