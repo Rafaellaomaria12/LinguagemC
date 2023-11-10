b082695955f823dbbd1c7176140723af6e2299e8
Rafaella <Rafaellamoliveira2005@gmail.com>
2023-11-10 11:54:49 -0300

    Criação do arquivo primeiro com variável numérica

---
+++ primeiro.c
@@ -0,0 +1,16 @@
+//utilização de variáveis
+//incluir a biblioteca stdio para entrada e saida de dados
+#include <stdio.h>
+int main(){
+      //declaração da variável valor com o tipo inteiro e número 10
+    int valor = 90;
+    printf("Meu irmão tem %d anos de idade\n",valor);
+
+    return 0;
+}
+
+
+
+
+
+
